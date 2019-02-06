c192cda2985ecd8115daf8e522ecdd073467e77d
Devinda <dsenanayaka1@gmail.com>
2019-02-05 22:16:35 -0600

    added recent option for meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,5 @@
 class Meal < ApplicationRecord
+  attribute :recent
   has_many :recipes, dependent: :destroy
   has_many :menus, dependent: :destroy
   has_many :ingredients, through: :recipes
@@ -10,7 +11,15 @@ class Meal < ApplicationRecord
   validates_uniqueness_of :name
 
   enum category: Settings.meal.category
-  
+
   scope :category, -> (category) { where category: category}
 
+  @@recent_meals =  Menu.where(created_at: 2.week.ago.beginning_of_week..1.week.ago.end_of_week).pluck(:meal_id).uniq.to_set
+  def recent
+    if @@recent_meals.include?(self.id)
+      return true
+    else
+      return false
+    end
+  end
 end
