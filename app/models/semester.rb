919d93671bfeca9378a124c6b040c7e2583922c4
Devinda Senanayaka <dsenanayaka1@gmail.com>
2019-08-25 17:36:01 -0500

    removed presence check for spring/fall since bool is evaled to false

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -5,7 +5,7 @@ class Semester < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :start
   validates_presence_of :end
-  validates_presence_of :spring, message: ' / Fall cannot be blank !'
+  # validates_presence_of :spring, message: ' / Fall cannot be blank !'
 
 
   #TODO make spring and year unique
